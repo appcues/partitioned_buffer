1c2fbaf9381fc38630cd2d7961cbc933fd7c669e
Andy LeClair <andyleclair@gmail.com>
2025-12-05 15:28:50 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PartitionedBuffer.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/appcues/partitioned_buffer"
 
   def project do
